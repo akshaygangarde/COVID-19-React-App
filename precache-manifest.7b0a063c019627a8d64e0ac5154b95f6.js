3a49566d006eae7725b0af61bc27f750d73ca2db
Akshay Ganagrde <akshay.ganagrde@gmail.com>
2020-04-25 11:19:28 +0530

    Updates

---
+++ precache-manifest.7b0a063c019627a8d64e0ac5154b95f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa0710441e43c6e2f449f4bb22fe6ac1",
+    "url": "/COVID-19-React-App/index.html"
+  },
+  {
+    "revision": "a4872e731790c68b2583",
+    "url": "/COVID-19-React-App/static/css/main.1feb02a5.chunk.css"
+  },
+  {
+    "revision": "d5c948ef9f1258d04d08",
+    "url": "/COVID-19-React-App/static/js/2.944cc076.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID-19-React-App/static/js/2.944cc076.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4872e731790c68b2583",
+    "url": "/COVID-19-React-App/static/js/main.ad5ce036.chunk.js"
+  },
+  {
+    "revision": "b23be2d832ce45451225",
+    "url": "/COVID-19-React-App/static/js/runtime-main.e30055a4.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID-19-React-App/static/media/image.d7265326.png"
+  }
+]);
